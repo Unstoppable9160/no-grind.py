d2fc6c3f0cc4de2e25a7b56499ad52543908b7ec
Unstoppable9160 <abyazjschool@gmail.com>
2021-12-05 20:09:14 +0400

    Fixed writeOnImg and added ImgOnImg

--- ImgManipulation/draw.py
+++ ImgManipulation/draw.py
@@ -1,7 +1,7 @@
 from PIL import Image, ImageFont, ImageDraw
 
 
-def writeOnImg(baseImgPath: str, outputFilePath: str, fontPath: str, fontSize: int, text1: str, x1: int, y1: int, hex1, text2: str, x2: int, y2: int, hex2):
+def writeOnImg(baseImgPath: str, outputFilePath: str, fontPath: str, fontSize: int, text1: str, x1: int, y1: int, hex1, text2: str = None, x2: int = None, y2: int = None, hex2=None):
     both = True
     if hex1 is None:
         hex1 = (0, 0, 0)
@@ -10,9 +10,18 @@ def writeOnImg(baseImgPath: str, outputFilePath: str, fontPath: str, fontSize: i
     if text2 is None:
         both = False
     baseImage = Image.open(baseImgPath)
-    Font = ImageFont.truetype(fontPath, fontSize)
+    font = ImageFont.truetype(fontPath, fontSize)
     draw = ImageDraw.Draw(baseImage)
-    draw.text((x1, y1), text1, hex1, font=Font)
+    draw.text((x1, y1), text1, hex1, font=font)
     if both:
-        draw.text((x2, y2), text2, hex2, font=Font)
+        draw.text((x2, y2), text2, hex2, font=font)
+    baseImage.save(outputFilePath)
+
+
+def drawImgOnImg(baseImgPath: str, outputFilePath: str, overlayImgPath: str, x: int, y: int, resize=None):
+    baseImage = Image.open(baseImgPath)
+    overlayImage = Image.open(overlayImgPath)
+    if resize != None:
+        overlayImage.resize(resize)
+    baseImage.paste(overlayImage, (x, y))
     baseImage.save(outputFilePath)
